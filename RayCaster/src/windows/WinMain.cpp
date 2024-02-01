aefb002929a3c57bead0640de38bf0fa3dc02443
Andrew <mrrobottrax@gmail.com>
2024-02-01 00:05:31 -0500

    Add gdiplus

--- RayCaster/src/windows/WinMain.cpp
+++ RayCaster/src/windows/WinMain.cpp
@@ -1,5 +1,22 @@
 #include <pch.h>
 
+using namespace Gdiplus;
+
+VOID OnPaint(HDC hdc)
+{
+	Graphics graphics(hdc);
+
+	Pen pen(Color(255, 0, 0, 255));
+	graphics.DrawLine(&pen, 0, 0, 200, 100);
+
+	FontFamily  fontFamily(L"Times New Roman");
+	Font        font(&fontFamily, 24, FontStyleRegular, UnitPixel);
+	PointF      pointF(30.0f, 10.0f);
+	SolidBrush  solidBrush(Color(255, 0, 0, 255));
+
+	graphics.DrawString(L"Hello", -1, &font, pointF, &solidBrush);
+}
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(
@@ -9,9 +26,10 @@ int WINAPI WinMain(
 	_In_ int nCmdShow
 )
 {
-	const wchar_t CLASS_NAME[] = L"Main Window Class";
-
+	// set up window class
 	WNDCLASS wc = {};
+
+	const wchar_t CLASS_NAME[] = L"Main Window Class";
 
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
@@ -20,18 +38,18 @@ int WINAPI WinMain(
 	RegisterClass(&wc);
 
 	HWND hwnd = CreateWindowEx(
-		0,                              // Optional window styles.
-		CLASS_NAME,                     // Window class
-		L"Game",						// Window text
-		WS_OVERLAPPEDWINDOW,            // Window style
+		0,                              // optional window styles.
+		CLASS_NAME,                     // window class
+		L"Game",						// window text
+		WS_OVERLAPPEDWINDOW,            // window style
 
 		// Size and position
 		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
 
-		NULL,       // Parent window    
-		NULL,       // Menu
-		hInstance,  // Instance handle
-		NULL        // Additional application data
+		NULL,       // parent window    
+		NULL,       // menu
+		hInstance,  // instance handle
+		NULL        // additional application data
 	);
 
 	if (hwnd == NULL)
@@ -39,10 +57,15 @@ int WINAPI WinMain(
 		return 0;
 	}
 
+	GdiplusStartupInput gdiplusStartupInput;
+	ULONG_PTR           gdiplusToken;
+
+	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+
 	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
 
-	// Run the message loop.
-
+	// run the message loop.
 	MSG msg = {};
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
 	{
@@ -50,6 +73,7 @@ int WINAPI WinMain(
 		DispatchMessage(&msg);
 	}
 
+	GdiplusShutdown(gdiplusToken);
 	return 0;
 }
 
@@ -66,9 +90,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			PAINTSTRUCT ps;
 			HDC hdc = BeginPaint(hwnd, &ps);
 
-			// All painting occurs here, between BeginPaint and EndPaint.
-
-			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+			//FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+			OnPaint(hdc);
 
 			EndPaint(hwnd, &ps);
 		}
