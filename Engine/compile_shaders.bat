d975cb49b1d400426a15a3295ece62d34b88480e
mrrobottrax <59744880+mrrobottrax@users.noreply.github.com>
2024-05-19 15:25:08 -0400

    Auto compile shaders

---
+++ Engine/compile_shaders.bat
@@ -0,0 +1,25 @@
+@echo off
+
+if exist %OUT_DIR%core\shaders (
+	echo Clearing shaders folder
+	rmdir /s /q %OUT_DIR%core\shaders
+)
+
+setlocal enabledelayedexpansion
+
+for /r %%f in (*.vert *.frag) do (
+	set FILE_PATH=%%~dpnxf
+	set FILE_DIR=%%~dpf
+
+	set RELATIVE_PATH=!FILE_PATH:%PROJ_DIR%=!
+	set RELATIVE_DIR=!FILE_DIR:%PROJ_DIR%=!
+
+	set OUTPUT_PATH=!OUT_DIR!!RELATIVE_PATH!
+	set OUTPUT_DIR=!OUT_DIR!!RELATIVE_DIR!
+
+	mkdir !OUTPUT_DIR!
+
+	glslc !FILE_PATH! -o !OUTPUT_PATH!.spv
+)
+
+endlocal
