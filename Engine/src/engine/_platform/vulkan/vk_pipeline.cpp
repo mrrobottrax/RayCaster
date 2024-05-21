dfe88b6721260a1621e6c3a9b1e5dfc7c6ea9b9c
Andrew <andrewjhoult@gmail.com>
2024-05-21 00:52:58 -0400

    Add shader modules

--- Engine/src/engine/_platform/vulkan/vk_pipeline.cpp
+++ Engine/src/engine/_platform/vulkan/vk_pipeline.cpp
@@ -3,8 +3,47 @@
 
 #include <_wrappers/file/file_wrapper.h>
 
+#include "vk_device.h"
+
+using namespace Vk;
+
+static VkShaderModule CreateShaderModule(const std::vector<char>& code)
+{
+	VkShaderModuleCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+	createInfo.codeSize = code.size();
+	createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data());
+
+	VkShaderModule shaderModule;
+	if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+		throw std::runtime_error(std::format("Failed to create shader module"));
+	}
+
+	return shaderModule;
+}
+
 void CreateGraphicsPipeline()
 {
 	auto vertShaderCode = ReadEntireFile("core/shaders/test.vert.spv");
-	//auto fragShaderCode = ReadEntireFile("core/shaders/frag.spv");
+	auto fragShaderCode = ReadEntireFile("core/shaders/test.frag.spv");
+
+	VkShaderModule vertShaderModule = CreateShaderModule(vertShaderCode);
+	VkShaderModule fragShaderModule = CreateShaderModule(fragShaderCode);
+
+	VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+	vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+	vertShaderStageInfo.module = vertShaderModule;
+	vertShaderStageInfo.pName = "main";
+
+	VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+	fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fragShaderStageInfo.module = fragShaderModule;
+	fragShaderStageInfo.pName = "main";
+
+	VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
+
+	vkDestroyShaderModule(device, vertShaderModule, nullptr);
+	vkDestroyShaderModule(device, fragShaderModule, nullptr);
 }
