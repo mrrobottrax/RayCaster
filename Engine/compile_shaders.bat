1150647a9eed6a79484c76af922f69d6d45da72f
Andrew <andrewjhoult@gmail.com>
2024-05-21 00:34:59 -0400

    Fix bugs with shader compilation

--- Engine/compile_shaders.bat
+++ Engine/compile_shaders.bat
@@ -3,26 +3,27 @@
 :: TODO: Only build when file has changed
 :: TODO: Compile to .obj files so the linker can insert them into executable??
 
-if exist %OUT_DIR%core\shaders (
+if exist "%OUT_DIR%core\shaders" (
 	echo Clearing shaders folder
-	rmdir /s /q %OUT_DIR%core\shaders
+	rmdir /s /q "%OUT_DIR%core\shaders"
 )
 
 setlocal enabledelayedexpansion
 
 for /r %%f in (*.vert *.frag) do (
-	set FILE_PATH=%%~dpnxf
-	set FILE_DIR=%%~dpf
+	set "FILE_PATH=%%~dpnxf"
+	set "FILE_DIR=%%~dpf"
 
-	set RELATIVE_PATH=!FILE_PATH:%PROJ_DIR%=!
-	set RELATIVE_DIR=!FILE_DIR:%PROJ_DIR%=!
+	set "RELATIVE_PATH=!FILE_PATH:%PROJ_DIR%=!"
+	set "RELATIVE_DIR=!FILE_DIR:%PROJ_DIR%=!"
 
-	set OUTPUT_PATH=!OUT_DIR!!RELATIVE_PATH!
-	set OUTPUT_DIR=!OUT_DIR!!RELATIVE_DIR!
+	set "OUTPUT_PATH=!OUT_DIR!!RELATIVE_PATH!"
+	set "OUTPUT_DIR=!OUT_DIR!!RELATIVE_DIR!"
 
-	mkdir !OUTPUT_DIR!
+	mkdir "!OUTPUT_DIR!"
 
-	glslc !FILE_PATH! -o !OUTPUT_PATH!.spv
+	echo "!FILE_PATH!"
+	glslc "!FILE_PATH!" -o "!OUTPUT_PATH!.spv"
 )
 
 endlocal
